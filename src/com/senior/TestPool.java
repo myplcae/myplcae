7cb1adbf44de63ba30eafab5914c05cc351231d1
HUAWEI <1479846498@qq.com>
2023-02-02 17:46:17 +0800

    改动

--- src/com/senior/TestPool.java
+++ src/com/senior/TestPool.java
@@ -25,8 +25,7 @@ class MyThread implements Runnable{
 
     @Override
     public void run() {
-        for (int i = 0; i < 4; i++) {
-            System.out.println(Thread.currentThread().getName()+i);
-        }
+            System.out.println(Thread.currentThread().getName());
+
     }
 }
