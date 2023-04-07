3a212426d0a2a0cad5132b4e0f6402913a47aa3d
HUAWEI <1479846498@qq.com>
2023-04-07 20:37:35 +0800

    true

--- src/LeedCodeStudy/Test.java
+++ src/LeedCodeStudy/Test.java
@@ -1,35 +1,36 @@
 package src.LeedCodeStudy;
 
-import java.util.Scanner;
-
-// 注意类名必须为 Main, 不要有任何 package xxx 信息
+import java.util.*;
 public class Test {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        // 注意 hasNext 和 hasNextLine 的区别
-        while (in.hasNextInt()) { // 注意 while 处理多个 case
-            String s1 = in.next();
-            String s2 = in.next();
-            //定义数组保存两串数
-            int arr[][] = new int[s1.length()+1][s2.length()+1];
-            //遍历相减
-            for(int i = 1;i<arr.length;i++){
-                arr[i][0] = i;
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            //定义输入的两个数组和一个用来定义两个数组的差
+            String s1 = sc.next();
+            String s2 = sc.next();
+            //定义输入的两串数
+            int dp[][] = new int[s1.length() + 1][s2.length() + 1];
+            dp[0][0] = 0;
+            //遍历第一串数
+            for (int i = 1; i < dp.length; i++) {
+                dp[i][0] = i;
             }
-            for(int i = 1;i<arr.length;i++){
-                arr[0][i] = i;
+            //遍历第二串数
+            for (int i = 1; i < dp[0].length; i++) {
+                dp[0][i] = i;
             }
-            //遍历相减
-            for(int i =1;i < arr.length;i++){
-                for(int j = 1;j<arr[0].length;j++){
-                    if(s1.charAt(i-1) == s2.charAt(j-1)){
-                        arr[i][j] = arr[i-1][j-1];
-                    }else{
-                        arr[i][j] = Math.min(arr[i-1][j-1]+1,Math.min(arr[i][j-1]+1,arr[i-1][j+1]));
+            //两串数位数通过遍历的方式相减
+            for (int i = 1; i < dp.length; i++) {
+                for (int j = 1; j < dp[0].length; j++) {
+                    if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    } else {
+                        dp[i][j] = Math.min(dp[i - 1][j - 1] + 1, Math.min(dp[i][j - 1] + 1,
+                                dp[i - 1][j] + 1));
                     }
                 }
             }
-            System.out.println(arr[s1.length()][s2.length()]);
+            System.out.println(dp[s1.length()][s2.length()]);
         }
     }
 }
