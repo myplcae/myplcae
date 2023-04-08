98bbe10a5a7b82419d3b9a076b4e85d63a3bc82e
HUAWEI <1479846498@qq.com>
2023-04-08 13:04:39 +0800

    false

--- src/LeedCodeStudy/Test.java
+++ src/LeedCodeStudy/Test.java
@@ -1,36 +1,46 @@
 package src.LeedCodeStudy;
 
-import java.util.*;
+import java.util.Scanner;
+
+// 注意类名必须为 Main, 不要有任何 package xxx 信息
 public class Test {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
-            //定义输入的两个数组和一个用来定义两个数组的差
-            String s1 = sc.next();
-            String s2 = sc.next();
-            //定义输入的两串数
-            int dp[][] = new int[s1.length() + 1][s2.length() + 1];
-            dp[0][0] = 0;
-            //遍历第一串数
-            for (int i = 1; i < dp.length; i++) {
-                dp[i][0] = i;
-            }
-            //遍历第二串数
-            for (int i = 1; i < dp[0].length; i++) {
-                dp[0][i] = i;
-            }
-            //两串数位数通过遍历的方式相减
-            for (int i = 1; i < dp.length; i++) {
-                for (int j = 1; j < dp[0].length; j++) {
-                    if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                        dp[i][j] = dp[i - 1][j - 1];
-                    } else {
-                        dp[i][j] = Math.min(dp[i - 1][j - 1] + 1, Math.min(dp[i][j - 1] + 1,
-                                dp[i - 1][j] + 1));
-                    }
-                }
-            }
-            System.out.println(dp[s1.length()][s2.length()]);
+        Scanner in = new Scanner(System.in);
+        // 注意 hasNext 和 hasNextLine 的区别
+        while (in.hasNext()) { // 注意 while 处理多个 case
+            int a = in.nextInt();
+            String x = in.next();
+            parseCmd(x, a);
         }
+    }
+    public static void parseCmd(String cmds, int n) {
+        //起始位置
+        int start = 1;
+        //页面末位置
+        int end = Math.min(n, 4);
+        //光标的位置，三个位置都是从1开始
+        int index = 1;
+        for (int i = 0; i < cmds.length(); i++) {
+            if (cmds.charAt(i) == 'U') {
+                index = (index - 1 - 1 + n) % n + 1;
+            } else if (cmds.charAt(i) == 'D') {
+                index = index % n + 1;
+            }
+            //判断滑动窗口的位置是否需要改变
+            if (index < start) {
+                //逛遍在窗口之上
+                start = index;
+                end = start + 3;
+            } else if (index > end) {
+                end = index;
+                start = end - 3;
+            }
+        }
+        //输出
+        for (int i = start ; i <= end; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println(index);
     }
 }
