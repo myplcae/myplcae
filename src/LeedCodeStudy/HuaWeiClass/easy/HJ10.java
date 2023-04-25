a322183dd5e3a6a1438e696ac427443234e53aa6
HUAWEI <1479846498@qq.com>
2023-04-25 22:13:42 +0800

    练习

---
+++ src/LeedCodeStudy/HuaWeiClass/easy/HJ10.java
@@ -0,0 +1,26 @@
+package src.LeedCodeStudy.HuaWeiClass.easy;
+
+import java.util.BitSet;
+import java.util.Scanner;
+
+/**
+ * 编写一个函数，计算字符串中含有的不同字符的个数。字符在 ASCII 码范围内( 0~127 ，包括 0 和 127 )，换行表示结束符，不算在字符里。不在范围内的不作统计。多个相同的字符只计算一次
+ * 例如，对于字符串 abaca 而言，有 a、b、c 三种不同的字符，因此输出 3 。
+ * 输入：输入一行没有空格的字符串。
+ * 输出：输出 输入字符串 中范围在(0~127，包括0和127)字符的种数。
+ * */
+public class HJ10 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String x = in.nextLine();
+        //通过BitSet函数去存字符串然后运算输出种数
+        BitSet bitSet = new BitSet(128);
+        //遍历剔除重复相同字符
+        for (char c : x.toCharArray()){
+            if (!bitSet.get(c)){
+                bitSet.set(c);
+            }
+        }
+        System.out.println(bitSet.cardinality());
+    }
+}
---
+++ src/LeedCodeStudy/HuaWeiClass/easy/HJ8.java
@@ -0,0 +1,34 @@
+package src.LeedCodeStudy.HuaWeiClass.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * 数据表记录包含表索引index和数值value（int范围的正整数），
+ * 请对表索引相同的记录进行合并，即将相同索引的数值进行求和运算，输出按照index值升序进行输出。
+ * 输入：先输入键值对的个数n（1 <= n <= 500）
+ * 接下来n行每行输入成对的index和value值，以空格隔开
+ * 输出：输出合并后的键值对（多行）
+ * 思路：用map封装输入键值对参数再进行相同key的value相加
+ * */
+public class HJ8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        Map<Integer, Integer> map = new HashMap<>(size);
+        for (int i = 0; i < size; i++) {
+            int key = scanner.nextInt();
+            int value = scanner.nextInt();
+            if (map.containsKey(key)) {
+                map.put(key, map.get(key) + value);
+            } else {
+                map.put(key, value);
+            }
+        }
+        for (Integer key : map.keySet()) {
+            System.out.println( key + " " + map.get(key));
+        }
+    }
+
+}
