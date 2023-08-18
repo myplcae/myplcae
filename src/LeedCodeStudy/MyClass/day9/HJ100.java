df52d7b267c1f1b8debb32559033688744959273
HUAWEI <1479846498@qq.com>
2023-08-18 17:24:35 +0800

    算法练习day9

---
+++ src/LeedCodeStudy/MyClass/day9/HJ100.java
@@ -0,0 +1,21 @@
+package src.LeedCodeStudy.MyClass.day9;
+
+import java.util.Scanner;
+
+/**
+ * 输入一个等差数列
+ * 输出等差数列的前n项和
+ * 输入：输入一个正整数n。
+ * 输出：输出一个相加后的整数
+ * */
+public class HJ100 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextInt()){
+            int length = in.nextInt();
+            int first = 2,last = 3*length-1;
+            System.out.println((first+last)*length/2);
+        }
+    }
+}
---
+++ src/LeedCodeStudy/MyClass/day9/HJ107.java
@@ -0,0 +1,32 @@
+package src.LeedCodeStudy.MyClass.day9;
+
+import java.util.Scanner;
+
+/**
+ * 求解立方根：计算一个浮点数的立方根，不使用库函数。保留一位小数。
+ * 输入：待求解参数，为double类型（一个实数）
+ * 输出：输出参数的立方根。保留一位小数。
+ * */
+public class HJ107 {
+    //直接用二分查找
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()){
+            double num = in.nextDouble();
+            double left = 0.0,right = num,mid = 0;
+            right = Math.max(1.0,num);
+            left = Math.min(-1.0,num);
+            while (right-left > 0.01){
+                mid = left + (right-left)/2;
+                if (mid * mid * mid < num){
+                    left = mid;
+                }else if (mid * mid * mid > num){
+                    right = mid;
+                }else {
+                    return;
+                }
+            }
+            System.out.printf("%.1f",mid);
+        }
+    }
+}
