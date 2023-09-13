b655dabafbfaada11d3cfe57cd9cc4ca6c3e7d61
HUAWEI <1479846498@qq.com>
2023-09-13 21:34:15 +0800

    算法练习代码03(漏掉的23题)

--- src/MyJaveStudy/BasicStudy/AlgorithmExercises/day2/HJ23.java
+++ src/MyJaveStudy/BasicStudy/AlgorithmExercises/day2/HJ23.java
@@ -1,8 +1,6 @@
 package src.MyJaveStudy.BasicStudy.AlgorithmExercises.day2;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * 实现删除字符串中出现次数最少的字符，若出现次数最少的字符有多个，
@@ -17,37 +15,27 @@ public class HJ23 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        HJ23 solution = new HJ23();
-
-        while (in.hasNextLine()){
+        while (in.hasNext()){
             String str = in.nextLine();
-            String result = solution.delete(str);
-            System.out.println(result);
+            char[] chars = str.toCharArray();//转成char字符集
+            //用hashMap去统计每种字符的数量
+            HashMap<Character,Integer> map = new HashMap<>();
+            //遍历添加进map
+            for (char c : chars) {
+                map.put(c,(map.getOrDefault(c,0) + 1));
+            }
+            //去找出现次数最少的字符，使用Collections
+            Collection<Integer> times = map.values();
+            Integer min = Collections.min(times);
+
+            //replaceall替换该字符为空
+            for (Character character : map.keySet()) {
+                if (map.get(character) == min){
+                    str = str.replaceAll(String.valueOf(character),"");
+                }
+            }
+            System.out.println(str);
         }
 
     }
-    //删除出现次数最少的字符
-    private String delete(String str) {
-
-        //定义一个集合记录某个字符出现的次数
-        Map<Character,Integer> map = new HashMap<>();
-        //遍历存入map中
-
-        for (char c: str.toCharArray()){
-            map.put(c,map.getOrDefault(c,0)+1);
-        }
-        //遍历这个map找出最小的一个key
-        int min = Integer.MAX_VALUE;
-        for (int times:map.values()) {
-            min =Math.min(min,times);
-        }
-        //删除出现次数最少的字符
-        StringBuilder sb = new StringBuilder();
-        for (char c:str.toCharArray()){
-            if (map.get(c) != min){
-                sb.append(c);
-            }
-        }
-        return sb.toString();
-        }
 }
